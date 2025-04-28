904fd84fef673d329b262c66f19e481cdbf2f559
polygrouplabs <dev.tblosh@gmail.com>
2025-04-28 00:56:55 -0500

    feature/ SEO metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata, Viewport } from "next";
-import "./globals.css";
 import { Montserrat } from "next/font/google";
 import { HeadlessClientContextProvider } from "@/context/headlessContext";
+
+import "./globals.css";
+import favicon from "./favicon.ico";
+
+const title = "BS Belinda store";
+const description =
+  "BS Belinda Store. Tu boutique de confianza, especializada en vestir a la mujer con prendas que combinan calidad, estilo y un toque de distinción.";
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -9,8 +15,32 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: "belinda-shop",
-  description: "belinda-shop",
+  metadataBase: new URL("https://bsbelindastore.com"),
+  title: title,
+  description: description,
+  openGraph: {
+    siteName: title,
+    title: title,
+    description: description,
+    countryName: "Colombia",
+    images: [
+      {
+        url: "/SEO.jpg",
+        width: 1916,
+        height: 982,
+      },
+    ],
+    locale: "es_ES",
+  },
+  icons: {
+    icon: favicon.src,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description,
+    images: ["/SEO.jpg"],
+  },
 };
 
 const montserrat = Montserrat({
@@ -24,7 +54,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="es" className="scroll-smooth">
+      <head>
+        <meta charSet="UTF-8" />
+
+        {/* Meta tags de Open Graph */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/SEO.jpg" />
+        <meta property="og:url" content="https://www.bsbelindastore.com" />
+        <meta property="og:image:width" content="1916" />
+        <meta property="og:image:height" content="982" />
+        <meta property="og:image:alt" content="BS Belinda - Tienda online" />
+        <meta
+          name="keywords"
+          content="Tienda virtual, e-commerce, vestidos, blusas, ropa para mujer"
+        />
+        <meta property="og:type" content="website" />
+
+        {/* Meta tags de Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="/SEO.jpg" />
+
+        <meta name="apple-mobile-web-app-title" content="BS Belinda" />
+      </head>
+
       <HeadlessClientContextProvider>
         <body className={`${montserrat.className} antialiased`}>
           {children}
